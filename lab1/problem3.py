7e092887ce04d2295ee6b5c6fa4cf4e099e1e473
Joshua Marasigan <marasigan.josh.villeno@gmail.com>
2017-01-24 09:43:37 -0600

    Covarience and Mean for mult Gaussian Dist.

--- lab1/problem3.py
+++ lab1/problem3.py
@@ -9,18 +9,25 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Mean Calculation
 def mean(n):
     return float(sum(n))/max(len(n),1)
 
+# Standard Deviation
+def std(n):
+    return math.sqrt(mean(abs(n-mean(n))**2))
+
+# Gaussian Distribution    
 mu, sigma = 0, 5
 s = np.random.normal(mu, sigma, 25000)
 
+# Mean of Gaussuan, should be ~0
 x = mean(s)
-y = math.sqrt(mean(abs(s - mean(s))**2))
 print("\n")
 print("Mean:  "+ str(x))
 
+# Standard Deviation of Gaussian Dist.
+y = std(s)
 print("\n")
 print("Standard Deviation:  " + str(y))
 

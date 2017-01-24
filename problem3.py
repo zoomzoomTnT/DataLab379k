c66240be1039b59a4edb9a7cf9327cd418d2cd10
Joshua Marasigan <marasigan.josh.villeno@gmail.com>
2017-01-24 08:48:43 -0600

    Done w 3

--- problem3.py
+++ problem3.py
@@ -5,15 +5,24 @@ Created on Fri Jan 20 10:42:36 2017
 @author: Josh M
 """
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def mean(n):
+    return float(sum(n)) / max(len(n), 1)
 
 mu, sigma = 0, 5
 s = np.random.normal(mu, sigma, 25000)
 
 plt.hist(s, 30, normed=True)
 
-x = np.mean(s)
-y = np.std(s)
+counter = 0
+for i in s:
+    counter += i
+
+x = counter/25000
+y = math.sqrt(mean(abs(s - mean(s))**2))
 print("mean: "+ str(x) + " sd: " + str(y))
 plt.show()

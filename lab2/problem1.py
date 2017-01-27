b8c7223a719b775846dbb13ab294a96ab233e7d6
Joshua Marasigan <marasigan.josh.villeno@gmail.com>
2017-01-27 10:29:40 -0600

    p1

--- lab2/problem1.py
+++ lab2/problem1.py
@@ -6,10 +6,23 @@ Created on Fri Jan 27 09:42:21 2017
 """
 
 import numpy as np
-import pandas as pd
+from pandas import *
+#from libraries.settings import *
+from scipy.stats.stats import pearsonr
+import itertools
 
 df = pd.read_csv('DF1.csv', usecols=[1,2,3,4])
 
+pairwisecorr = {}
 df.columns = ['col1', 'col2', 'col3', 'col4']
+col = df.columns.tolist()
 
 print(df)
+
+for col_a, col_b in itertools.combinations(col, 2):
+    pairwisecorr[col_a + '__' + col_b] = pearsonr(df.loc[:, col_a], df.loc[:, col_b])
+
+result = DataFrame.from_dict(pairwisecorr, orient='index')
+result.columns = ['PCC', 'p-value']
+
+print(result.sort_index())
